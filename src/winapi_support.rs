e1103b541ec3c7db8078cd058409bdddfb52cf07
Redrield <redrield@gmail.com>
2018-10-07 08:47:38 -0400

    other way around test

--- src/winapi_support.rs
+++ src/winapi_support.rs
@@ -37,11 +37,26 @@ impl Uuid {
 mod tests {
     use prelude::*;
     use std::str::FromStr;
+    use winapi::shared::guiddef;
 
     #[test]
-    fn test_conversion() {
+    fn test_to_uuid() {
         let uuid = Uuid::from_str("735d359d-4bc4-4e07-8c49-eb3e99a048dc").unwrap();
         let guid = uuid.to_guid();
         assert_eq!(Ok(uuid), Uuid::from_guid(guid));
+    }
+
+    #[test]
+    fn test_from_guid() {
+        let guid = guiddef::GUID {
+            Data1: 0x4a35229d,
+            Data2: 0x5527,
+            Data3: 0x4f30,
+            Data4: [0x86, 0x47, 0x9d, 0xc5, 0x4e, 0x1e, 0xe1, 0xe8]
+        };
+
+        let uuid = Uuid::from_guid(guid).unwrap();
+        assert_eq!("4a35229d-5527-4f30-8647-9dc54e1ee1e8", uuid.to_hyphenated().to_string());
+        assert_eq!(guid, uuid.to_guid());
     }
 }
